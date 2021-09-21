e42c050f6e2e16b2836d232a00d91461c923196d
zinstack625 <koala.klimanov@gmail.com>
2021-09-21 15:05:52 +0300

    lab-03: lab done

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,72 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Klimanov Anton <klimanov625@outlook.com>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#include <atomic>
+#include <cstddef>
+#include <utility>
+
+template <class T>
+class SharedPtr {
+ public:
+  SharedPtr() : data(new T()), ref_cnt(new std::atomic<size_t>(1)) {}
+  explicit SharedPtr(T* ptr) : data(ptr), ref_cnt(new std::atomic<size_t>(1)) {}
+  SharedPtr(const SharedPtr& r) : data(r.data), ref_cnt(r.ref_cnt) {
+    ++(*r.ref_cnt);
+  }
+  SharedPtr(SharedPtr&& r) {
+    data = r.data;
+    ref_cnt = r.ref_cnt;
+    r.data = nullptr;
+    r.ref_cnt = new std::atomic<size_t>(1);
+  }
+  ~SharedPtr() {
+    if (--(*ref_cnt) == 0) {
+      delete data;
+      delete ref_cnt;
+    }
+    data = nullptr;
+    ref_cnt = nullptr;
+  }
+  auto operator=(const SharedPtr& r) -> SharedPtr& {
+    data = r.data;
+    ref_cnt = r.ref_cnt;
+    ++(*ref_cnt);
+    return *this;
+  }
+  auto operator=(SharedPtr&& r) -> SharedPtr& {
+    data = std::move(r.data);
+    *ref_cnt = *r.ref_cnt;
+    return *this;
+  }
+
+  // проверяет, указывает ли указатель на объект
+  operator bool() const { return data; }
+  auto operator*() const -> T& { return *data; }
+  auto operator->() const -> T* { return data; }
+
+  auto get() -> T* { return data; }
+  void reset() {
+    if (--(*ref_cnt) == 0) {
+      delete data;
+      delete ref_cnt;
+    }
+    data = nullptr;
+    ref_cnt = new std::atomic<size_t>(1);
+  }
+  void reset(T* ptr) { data = ptr; }
+  void swap(SharedPtr& r) {
+    std::swap(data, r.data);
+    std::swap(ref_cnt, r.ref_cnt);
+  }
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же
+  // управляемый объект
+  auto use_count() const -> size_t { return *ref_cnt; }
+
+ private:
+  T* data;
+  std::atomic<size_t>* ref_cnt;
+};
+
+#endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,69 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Klimanov Anton <klimanov625@outlook.com>
 
 #include <gtest/gtest.h>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include "header.hpp"
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(SharedPtr, Construct) {
+    auto shared_ptr = SharedPtr<int>();
+    *shared_ptr = 0;
+    EXPECT_EQ(*shared_ptr, 0);
+    for (size_t i = 0; i < 1000; ++i) {
+      *shared_ptr = i;
+      EXPECT_EQ(*shared_ptr, i);
+    }
+    shared_ptr.reset();
+}
+
+TEST(SharedPtr, Copy_Move) {
+    SharedPtr<int> shared_ptr(new int);
+    *shared_ptr = 317289;
+    EXPECT_EQ(*shared_ptr, 317289);
+
+    auto copy(shared_ptr);
+    auto copy2 = shared_ptr;
+    auto moved = std::move(shared_ptr);
+
+    EXPECT_EQ(shared_ptr.get(), nullptr);
+    ASSERT_EQ(copy, moved);
+    ASSERT_EQ(copy2, moved);
+    EXPECT_EQ(moved.use_count(), 3);
+    EXPECT_EQ(copy.use_count(), 3);
+    EXPECT_EQ(copy2.use_count(), 3);
+    copy.reset();
+    EXPECT_FALSE(bool(copy));
+    EXPECT_EQ(moved.use_count(), 2);
+    EXPECT_EQ(copy2.use_count(), 2);
+}
+
+TEST(SharedPtr, Swap) {
+  SharedPtr<double> somevalue(new double(2.0));
+  SharedPtr<double> othervalue(new double(1.0));
+  EXPECT_EQ(*somevalue, 2.0);
+  EXPECT_EQ(*othervalue, 1.0);
+  somevalue.swap(othervalue);
+  EXPECT_EQ(*somevalue, 1.0);
+  EXPECT_EQ(*othervalue, 2.0);
+}
+
+std::mutex mut;
+void inc_counter(SharedPtr<size_t> counter) {
+  mut.lock();
+  ++(*counter);
+  mut.unlock();
+}
+
+TEST(SharedPtr, Threads) {
+  SharedPtr<size_t> counter(new size_t(0));
+  std::vector<std::thread> threads;
+  threads.reserve(1000);
+  for (size_t i = 0; i < 1000; ++i) {
+     threads.push_back(std::thread(inc_counter, counter));
+  }
+  for (size_t i = 0; i < 1000; ++i) {
+    threads.at(i).join();
+  }
+  EXPECT_EQ(*counter, 1000);
 }
